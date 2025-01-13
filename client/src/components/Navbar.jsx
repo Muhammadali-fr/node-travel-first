7a1e1be2064e4f53402b211f2c33b08e60277060
Muhamadali-fr <boburjonr934@gmail.com>
2025-01-13 23:43:08 +0500

    second commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-full'>
+            <nav>
+                <ul>
+                    <li><a href="#">Travel books</a></li>
+                    <li><a href="#">add book</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
